446cc90d1643bc3fb50dc748436460dc7a33aac7
sddrouet@espe.edu.ec <sddrouet@espe.edu.ec>
2024-09-05 22:37:47 -0500

    ideas page and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/ideas', [IdeaController::class, 'index'])->name('idea.index');
